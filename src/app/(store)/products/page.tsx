cb731bc53c5e0ae2a1f41e787ea0db8d1130c3cf
amnesia2k <tilewaolatoye17@gmail.com>
2025-06-30 11:51:09 +0100

    fix: added Suspence useSearchParam on products page

--- src/app/(store)/products/page.tsx
+++ src/app/(store)/products/page.tsx
@@ -1,13 +1,25 @@
-import React, { Suspense } from "react";
+// import React from "react";
+// import Products from "./products-page";
+
+// export default function ProductsPage({
+//   searchParams,
+// }: {
+//   searchParams: { category?: string };
+// }) {
+//   return (
+//     <div className="p-5 md:px-10 md:py-5">
+//       <Products selectedCategory={searchParams.category} />
+//     </div>
+//   );
+// }
+
+import React from "react";
 import Products from "./products-page";
-import Loader from "@/components/loader";
 
 export default function ProductsPage() {
   return (
     <div className="p-5 md:px-10 md:py-5">
-      <Suspense fallback={<Loader />}>
-        <Products />
-      </Suspense>
+      <Products />
     </div>
   );
 }
